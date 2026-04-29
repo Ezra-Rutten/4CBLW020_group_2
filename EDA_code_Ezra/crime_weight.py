d0937b548b47746ae122c51a0781699fac38223e
Ezra Rutten <e.a.p.rutten@student.tue.nl>
2026-04-29 10:41:52 +0200

    Upload EDA

---
+++ EDA_code_Ezra/crime_weight.py
@@ -0,0 +1,17 @@
+# Currently not based on research, but filled in "randomly"
+crime_weights = {
+    "Anti-social behaviour": 1,
+    "Bicycle theft": 1,
+    "Burglary": 3,  
+    "Criminal damage and arson": 2,
+    "Drugs": 3,
+    "Other crime": 2,
+    "Other theft": 2,
+    "Possession of weapons": 4,
+    "Public order": 2,
+    "Robbery": 5,
+    "Shoplifting": 1,
+    "Theft from the person": 3,   
+    "Vehicle crime": 3,    
+    "Violence and sexual offences": 6 
+}
