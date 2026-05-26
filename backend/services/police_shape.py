8602a7f04458d95c40b0fbfba6aa5013081dc0ba
Ezra Rutten <e.a.p.rutten@student.tue.nl>
2026-05-26 15:09:54 +0200

    name change

--- backend/services/police_shape.py
+++ backend/services/police_shape.py
@@ -1,10 +1,24 @@
 import requests
 import csv
 import time
+import pandas as pd
+from shapely.geometry import Point
 import requests
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import json
 import csv
 import time
-from pathlib import Path
+import folium
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import glob
+import os
+import geopandas as gpd
+import pandas as pd
+from shapely.geometry import Point
 
 stations = [
     ("Acton Police Station", "W3 9BH"),
@@ -43,8 +57,7 @@ stations = [
     ("Wimbledon Police Station", "SW19 8NN")
 ]
 
-Path("../data/shape").mkdir(parents=True, exist_ok=True)
-OUTPUT_FILE = "../data/shape/metropolitan_police_stations.csv"
+OUTPUT_FILE = "metropolitan_police_stations.csv"
 
 with open(OUTPUT_FILE, "w", newline="", encoding="utf-8") as csvfile:
     writer = csv.writer(csvfile)
@@ -64,3 +77,42 @@ with open(OUTPUT_FILE, "w", newline="", encoding="utf-8") as csvfile:
         time.sleep(0.2)
     
 print(f"\nDataset written to {OUTPUT_FILE}")
+
+# Reading LSOA shapes
+base_path = "../data/LB_shp"
+shapefiles = glob.glob(os.path.join(base_path, "**/*.shp"), recursive=True)
+print(f"Found {len(shapefiles)} borough shapefiles")
+lsoa_gdfs = [gpd.read_file(shp) for shp in shapefiles]
+lsoa_london = gpd.GeoDataFrame(
+    pd.concat(lsoa_gdfs, ignore_index=True),
+    crs=lsoa_gdfs[0].crs
+)
+
+lsoa_london = lsoa_london.to_crs(epsg=27700)
+
+lsoa_london["centroid"] = lsoa_london.geometry.centroid
+lsoa_centroids = lsoa_london.set_geometry("centroid")
+
+stations_df = pd.read_csv("metropolitan_police_stations.csv")
+
+stations_gdf = gpd.GeoDataFrame(
+    stations_df,
+    geometry=gpd.points_from_xy(
+        stations_df.longitude,
+        stations_df.latitude
+    ),
+    crs="EPSG:4326"
+).to_crs(epsg=27700)
+
+lsoa_with_station = gpd.sjoin_nearest(
+    lsoa_centroids,
+    stations_gdf,
+    how="left",
+    distance_col="distance_to_station"
+)
+
+lsoa_station_map = lsoa_with_station[
+    ["lsoa21cd", "station", "distance_to_station"]
+]
+
+lsoa_station_map.to_csv("lsoa_station_map.csv", index=False)
