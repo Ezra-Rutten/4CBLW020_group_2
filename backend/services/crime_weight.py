1607ea11da2f108794bae886bcc7f1fc1752030b
Ezra Rutten <e.a.p.rutten@student.tue.nl>
2026-06-02 13:45:41 +0200

    Add specialist severity

--- backend/services/crime_weight.py
+++ backend/services/crime_weight.py
@@ -1,4 +1,6 @@
 # Based on CCHI index median
+
+# General patrol officer 
 crime_weights = {
   "Violence and sexual offences": 182,
   "Robbery": 365,
@@ -14,4 +16,92 @@ crime_weights = {
   "Shoplifting": 1,
   "Bicycle theft": 2,
   "Anti-social behaviour": 19
+}
+
+# Specialist 0-10
+
+crime_weights_mental_health = {
+  "Violence and sexual offences": 5,
+  "Robbery": 0,
+  "Burglary": 0,
+  "Possession of weapons": 0,
+  "Drugs": 5,
+  "Criminal damage and arson": 5,
+  "Public order": 5,
+  "Other crime": 0,
+  "Other theft": 0,
+  "Theft from the person": 0,
+  "Vehicle crime": 0,
+  "Shoplifting": 0,
+  "Bicycle theft": 0,
+  "Anti-social behaviour": 5
+}
+
+crime_weights_social_services = {
+  "Violence and sexual offences": 5,
+  "Robbery": 0,
+  "Burglary": 0,
+  "Possession of weapons": 0,
+  "Drugs": 5,
+  "Criminal damage and arson": 0,
+  "Public order": 0,
+  "Other crime": 0,
+  "Other theft": 0,
+  "Theft from the person": 0,
+  "Vehicle crime": 0,
+  "Shoplifting": 0,
+  "Bicycle theft": 0,
+  "Anti-social behaviour": 5
+}
+
+crime_weights_negotiator = {
+  "Violence and sexual offences": 5,
+  "Robbery": 0,
+  "Burglary": 0,
+  "Possession of weapons": 5,
+  "Drugs": 0,
+  "Criminal damage and arson": 0,
+  "Public order": 5,
+  "Other crime": 0,
+  "Other theft": 0,
+  "Theft from the person": 0,
+  "Vehicle crime": 0,
+  "Shoplifting": 0,
+  "Bicycle theft": 0,
+  "Anti-social behaviour": 0
+}
+
+
+crime_weights_k9 = {
+  "Violence and sexual offences": 0,
+  "Robbery": 0,
+  "Burglary": 5,
+  "Possession of weapons": 5,
+  "Drugs": 10,
+  "Criminal damage and arson": 0,
+  "Public order": 0,
+  "Other crime": 0,
+  "Other theft": 0,
+  "Theft from the person": 0,
+  "Vehicle crime": 5,
+  "Shoplifting": 5,
+  "Bicycle theft": 0,
+  "Anti-social behaviour": 0
+}
+
+crime_weights_swat = {
+  "Violence and sexual offences": 10,
+  "Robbery": 5,
+  "Burglary": 0,
+  "Possession of weapons": 10,
+  "Drugs": 0,
+  "Criminal damage and arson": 5,
+  "Public order": 0,
+  "Other crime": 0,
+  "Other theft": 0,
+  "Theft from the person": 0,
+  "Vehicle crime": 0,
+  "Shoplifting": 0,
+  "Bicycle theft": 0,
+  "Anti-social behaviour": 0
 }
